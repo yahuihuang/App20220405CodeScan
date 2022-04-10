d0a96992af0f088480010c7582b37e7388005ed9
Huang Grace <huang.yahui.grace@gmail.com>
2022-04-10 10:54:52 +0800

    顯示相機

--- App20220405CodeScan/ViewController.swift
+++ App20220405CodeScan/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var myView: UIView!
@@ -22,9 +22,39 @@ class ViewController: UIViewController {
     }
 
     func setCodeScan() {
+        // 設定輸出設備
         guard let avCaptureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
         guard let avCaptureInput = try? AVCaptureDeviceInput(device: avCaptureDevice) else { return }
         avCaptureSession.addInput(avCaptureInput)
+        
+        // 設定輸出代理
+        // 逎立輸出物件，並設定ViewController為代理人
+        let avCaptureMetadataOutput = AVCaptureMetadataOutput()
+        // 輸出時，使用主線程
+        avCaptureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        // session加上輸出
+        avCaptureSession.addOutput(avCaptureMetadataOutput)
+        
+        // 輸出需指定類別
+        avCaptureMetadataOutput.metadataObjectTypes
+            = [AVMetadataObject.ObjectType.qr,
+               AVMetadataObject.ObjectType.code128,
+               AVMetadataObject.ObjectType.code39,
+               AVMetadataObject.ObjectType.code93,
+               AVMetadataObject.ObjectType.upce,
+               AVMetadataObject.ObjectType.pdf417,
+               AVMetadataObject.ObjectType.ean13,
+               AVMetadataObject.ObjectType.aztec]
+        
+        // 建一UIView做為輸出的影像，把圖層加到畫面中的圖層，讓使用者看到
+        let avCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: avCaptureSession)
+        avCaptureVideoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        avCaptureVideoPreviewLayer.frame = myView.bounds
+        self.myView.layer.addSublayer(avCaptureVideoPreviewLayer)
+        
+        // 第一次啟動
+        avCaptureSession.startRunning()
+        
     }
 
 }
